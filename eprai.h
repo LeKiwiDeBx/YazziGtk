97228e3076423216f331db4057eedc9c102a36e0
LeKiwiDeBx <tatareau@free.fr>
2020-01-01 18:58:43 +0100

    AI et fichier batch

--- eprai.h
+++ eprai.h
@@ -7,11 +7,11 @@
 #ifndef _EPRAI_H_
 #define _EPRAI_H_
 
-#include <stdio.h>  
-#include <gtk/gtk.h>  
+#include <stdio.h>
+#include <gtk/gtk.h>
 #include "dice_engine.h"
 
 //public
-//~ void epr_mediator_engine() ;
-diceSet* _epr_get_set_dices();
+// void epr_mediator_engine() ;
+diceSet *_epr_get_set_dices();
 #endif
